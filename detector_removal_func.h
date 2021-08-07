b46a09eb63566082852990817a23763b428429cb
Graph Inc <68469953+graph-inc@users.noreply.github.com>
2021-08-07 11:07:00 +0430

    new methods
    new heuristic detection methods

---
+++ detector_removal_func.h
@@ -0,0 +1,39 @@
+#pragma once
+
+
+#include <windows.h>
+#include <tlhelp32.h>
+#include <Shlwapi.h>
+
+
+
+class IOC;
+
+int generic_removal(IOC&);
+int generic_detector(IOC&);
+
+
+typedef struct _PROCESS_BASIC_INFORMATION
+{
+	LONG ExitStatus;
+	PVOID PebBaseAddress;
+	ULONG_PTR AffinityMask;
+	LONG BasePriority;
+	ULONG_PTR UniqueProcessId;
+	ULONG_PTR ParentProcessId;
+} PROCESS_BASIC_INFORMATION, *PPROCESS_BASIC_INFORMATION;
+
+typedef struct _UNICODE_STRING
+{
+	USHORT Length;
+	USHORT MaximumLength;
+	PWSTR Buffer;
+} UNICODE_STRING, *PUNICODE_STRING;
+
+typedef NTSTATUS(NTAPI *_NtQueryInformationProcess)(
+	HANDLE ProcessHandle,
+	DWORD ProcessInformationClass,
+	PVOID ProcessInformation,
+	DWORD ProcessInformationLength,
+	PDWORD ReturnLength
+	);
